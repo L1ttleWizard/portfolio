ee4c511189977f8359bb69aaca2baf46607838c5
L1ttleWizard <andrwyandreev2005@gmail.com>
2022-11-06 21:31:41 +0300

    heeeelp

--- scripts/script_vanilla.js
+++ scripts/script_vanilla.js
@@ -3,9 +3,9 @@ console.log(portfolioImgs);
 for(var i=0;i<portfolioImgs.length;i++) {
     console.log(portfolioImgs[i]);
     i=i+1;
-    let url='url(../images/portfolio-image-'+i+'.jpg)';
+    let imageLink='../images/portfolio-image-'+i+'.jpg';
     i=i-1;
     console.log(portfolioImgs[i].style);
-    portfolioImgs[i].style.backgroundImage=url;
+    portfolioImgs[i].style.backgroundImage=`url(${imageLink})`;
     portfolioImgs[i].style.backgroundPosition='center center';
 }
